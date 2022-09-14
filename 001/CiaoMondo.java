152dbc0972bdabfccd0f6988eb80152e6d907b73
Filippo <filippo.bilardo@tiscali.it>
2022-09-14 20:19:25 +0200

    Create CiaoMondo.java

---
+++ 001/CiaoMondo.java
@@ -0,0 +1,9 @@
+// Il nostro primo programma
+
+//Il nome della classe è uguale al nome del file
+class CiaoMondo {
+    public static void main(String[] args) {
+        System.out.println("Ciao, Mondo!"); 
+        
+    }
+}
