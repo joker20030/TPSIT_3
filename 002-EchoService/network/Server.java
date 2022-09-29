ad9947ac9dc3ba2921e00656030d9340585fe338
Filippo <filippo.bilardo@tiscali.it>
2022-09-29 18:30:44 +0200

    Update Server.java

--- 002-EchoService/network/Server.java
+++ 002-EchoService/network/Server.java
@@ -11,26 +11,29 @@ import java.net.Socket;
 
 public class Server {
 	public static void main(String[] args) throws Exception {
+		int severPort=8698;
+		String clientMessage = "";
+		
 		try {			 
-			int severPort=8698;
-			String clientMessage = "";
-			
 			// Listen to port
 			ServerSocket server = new ServerSocket(severPort);
-			System.out.println("Server: in ascolto sula porta " + severPort );
+			System.out.println("Server: in ascolto sula porta " + severPort);
 			Socket serverClientSocket = server.accept();
 			
+			// Input/Output dei dati
 			DataInputStream inStream = new DataInputStream(serverClientSocket.getInputStream());
 			DataOutputStream outStream = new DataOutputStream(serverClientSocket.getOutputStream());	
-			
+
+			// Scambio di dati tra client e server
 			while(!clientMessage.equals("end")) {
-		      clientMessage = inStream.readUTF();
-		      System.out.println("Server: ricevuto messaggio " + clientMessage );
-		      System.out.println("Server: invio messaggio "    + clientMessage );
-			  outStream.writeUTF("Echo from server : "         + clientMessage);
-		      outStream.flush();
+				clientMessage = inStream.readUTF();
+				System.out.println("Server: ricevuto messaggio " + clientMessage );
+				System.out.println("Server: invio messaggio "    + clientMessage );
+				outStream.writeUTF("Echo from server : "         + clientMessage);
+				outStream.flush();
 			}
 
+			// Chiusura del socket
 			server.close();
 		} catch (Exception e) {
 			System.out.println(e);
