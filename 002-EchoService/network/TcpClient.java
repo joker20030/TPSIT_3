a6d9b2e1f6e2875c046cbec366eadc2a1c2c2e9f
Filippo <filippo.bilardo@tiscali.it>
2022-10-01 11:00:29 +0200

    Update and rename Client.java to TcpClient.java

--- 002-EchoService/network/TcpClient.java
+++ 002-EchoService/network/TcpClient.java
@@ -1,7 +1,7 @@
 /**
  * from network/..
- * javac network/Client.java
- * java network.Client */
+ * javac network/TcpClient.java
+ * java network.TcpClient */
 
 package network;
 
@@ -10,31 +10,34 @@ import java.io.DataOutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Client {
+public class TcpClient {
 	public static void main(String[] args) throws Exception {
 		
 		String severAddress="127.0.0.1";  // localhost
-		int severPort=8698;
+		int severPort=8765;
 		String clientMessage = "";
 		String serverMessage = "";
 		
 		try {
 			// Create connection to server socket
-			System.out.print("Client: Tentativo di connessione server=" + severAddress + ":" + severPort + " ... ");
-			Socket socket = new Socket(severAddress, severPort); //
+			System.out.print("Client: Connessione al server=" + severAddress + ":" + severPort + " ... ");
+			Socket socket = new Socket(severAddress, severPort); 
 			System.out.println("Connected");
 
-			// Create streams to read/write data
-			DataInputStream inStream = new DataInputStream(socket.getInputStream());
-			DataOutputStream outStream = new DataOutputStream(socket.getOutputStream());
-
+			// Create input and output streams to read/write data
+			// Input stream tramite la classe java Scanner per i dati inseriti dall'utente 
+			//BufferedReader inUserStream = new BufferedReader(new InputStreamReader(System.in));
 			Scanner scanner = new Scanner(System.in);
+			// Input stream per i dati provenienti dal socket 
+			DataInputStream inStream = new DataInputStream(socket.getInputStream());
+			// Output stream 
+			DataOutputStream outStream = new DataOutputStream(socket.getOutputStream());
 			
 			while (!clientMessage.equals("end")) {
-
 				// Prompt user to enter some number or 'end'
 				System.out.print("Client: inserisci il messaggio da inviare> ");
 				clientMessage = scanner.nextLine();
+				//clientMessage = inUserStream.readLine();
 
 				// Send the entered number to server
 				System.out.println("Client: invio il messaggio: " + clientMessage);
