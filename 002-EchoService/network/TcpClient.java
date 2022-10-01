0bb52a06f6216503ebd03bfe54ee4a8760a319d1
Filippo <filippo.bilardo@tiscali.it>
2022-10-01 09:23:35 +0000

    update

--- 002-EchoService/network/TcpClient.java
+++ 002-EchoService/network/TcpClient.java
@@ -2,21 +2,22 @@
  * from network/..
  * javac network/TcpClient.java
  * java network.TcpClient */
-
 package network;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.Socket;
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class TcpClient {
 	public static void main(String[] args) throws Exception {
 		
 		String severAddress="127.0.0.1";  // localhost
 		int severPort=8765;
-		String clientMessage = "";
-		String serverMessage = "";
+		String clientMsg = "";
+		String serverMsg = "";
 		
 		try {
 			// Create connection to server socket
@@ -26,34 +27,35 @@ public class TcpClient {
 
 			// Create input and output streams to read/write data
 			// Input stream tramite la classe java Scanner per i dati inseriti dall'utente 
-			//BufferedReader inUserStream = new BufferedReader(new InputStreamReader(System.in));
-			Scanner scanner = new Scanner(System.in);
+			BufferedReader inUserStream = new BufferedReader(new InputStreamReader(System.in));
+			//Scanner scanner = new Scanner(System.in);
 			// Input stream per i dati provenienti dal socket 
 			DataInputStream inStream = new DataInputStream(socket.getInputStream());
 			// Output stream 
 			DataOutputStream outStream = new DataOutputStream(socket.getOutputStream());
 			
-			while (!clientMessage.equals("end")) {
-				// Prompt user to enter some number or 'end'
+			while (!clientMsg.equals("quit")) {
+				// Prompt user to enter some text or 'quit'
 				System.out.print("Client: inserisci il messaggio da inviare> ");
-				clientMessage = scanner.nextLine();
-				//clientMessage = inUserStream.readLine();
+				//clientMsg = scanner.nextLine();
+				clientMsg = inUserStream.readLine();
 
 				// Send the entered number to server
-				System.out.println("Client: invio il messaggio: " + clientMessage);
-				outStream.writeUTF(clientMessage);
+				System.out.println("Client: invio il messaggio: " + clientMsg);
+				outStream.writeUTF(clientMsg);
 				outStream.flush();
 
 				// Read data from socket input stream
-				serverMessage = inStream.readUTF();
-				System.out.println("Client: ricevuto il messaggio: " + serverMessage);
+				serverMsg = inStream.readUTF();
+				System.out.println("Client: ricevuto il messaggio: " + serverMsg);
 			}
 
 			// Close resources
 			outStream.close();
-			outStream.close();
+			inStream.close();
 			socket.close();
-			scanner.close();			
+			inUserStream.close();
+			//scanner.close();			
 		} catch (Exception e) {
 			System.out.println(e);
 		}
