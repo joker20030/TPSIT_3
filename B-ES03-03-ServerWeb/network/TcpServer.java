22c89d5d685f7bf31db56b63cee3d2a83771f99c
Filippo <filippo.bilardo@tiscali.it>
2022-11-24 23:04:59 +0000

    u

--- B-ES03-03-ServerWeb/network/TcpServer.java
+++ B-ES03-03-ServerWeb/network/TcpServer.java
@@ -22,42 +22,51 @@ public class TcpServer {
 			ServerSocket serverSocket = new ServerSocket(severPort);
 			System.out.println("Server: in ascolto sulla porta " + severPort);
 
-			// Attesa della connessione con il client
-			System.out.println("Attesa ricezione dati dal client ....................... \n");
-			Socket clientSocket = serverSocket.accept();
-			
-			// Create output stream to write data
-            //PrintWriter outStream = new PrintWriter(clientSocket.getOutputStream(), true);   
-			DataOutputStream outStream = new DataOutputStream(clientSocket.getOutputStream());	
-			// Create input stream to read data from socket
-			BufferedReader inStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			
-            // Ciclo di ricezione dal client e echo
-			//Lettura dati dal client un righa alla volta   
-			int i=1; 
-            while ((clientMsg=inStream.readLine()) != null && clientMsg.length()!=0) {
-				//Riga di dati ricevuta dal client
-				//System.out.println(i++ + " " + clientMsg + "-" + clientMsg.length());	
-				System.out.println(clientMsg);	
-			}  
+			Socket clientSocket;
 
-			//Invio echo dati su stream di rete
-			clientMsg = "HTTP/1.1 200 OK\r\n";
-			clientMsg += "Connection: close\r\n";
-			clientMsg += "Content-Type: text/plain\r\n";
-			clientMsg += "\r\n";
-			clientMsg += "Saluti dal web server Java\r\n";
-			outStream.writeChars(clientMsg);
-			outStream.flush();
-			//outStream.print(clientMsg);    
-		
-			System.out.println("\n....................... Fine ricezione dati\n");
+			boolean endConn=false;
+			while(!endConn){
+				// Attesa della connessione con il client
+				System.out.println("Attesa ricezione dati dal client ....................... \n");
+				clientSocket = serverSocket.accept();
+				
+				// Create output stream to write data
+				//PrintWriter outStream = new PrintWriter(clientSocket.getOutputStream(), true);   
+				DataOutputStream outStream = new DataOutputStream(clientSocket.getOutputStream());	
+				// Create input stream to read data from socket
+				BufferedReader inStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				
+				// Ciclo di ricezione dal client e echo
+				//Lettura dati dal client un righa alla volta   
+				int i=1; 
+				while ((clientMsg=inStream.readLine()) != null && clientMsg.length()!=0) {
+					//Riga di dati ricevuta dal client
+					//System.out.println(i++ + " " + clientMsg + "-" + clientMsg.length());	
+					System.out.println(clientMsg);	
+				}  
+
+				//Invio echo dati su stream di rete
+				clientMsg = "HTTP/1.0 200 OK\n\n";
+				clientMsg += "Saluti dal web server Java";
+				/*
+				//Invio echo dati su stream di rete
+				clientMsg = "HTTP/1.1 200 OK\r\n";
+				clientMsg += "Connection: close\r\n";
+				clientMsg += "Content-Type: text/plain\r\n";
+				clientMsg += "\r\n";
+				clientMsg += "Saluti dal web server Java\r\n"; */
+				outStream.write(clientMsg.getBytes());
+				outStream.flush();
+				//outStream.print(clientMsg);    
+			
+				System.out.println("\n....................... Fine ricezione dati\n");
+				clientSocket.close();
+				inStream.close();
+				outStream.close();
+			}
 
 			// Close resources
 			serverSocket.close();
-			clientSocket.close();
-			inStream.close();
-			outStream.close();
 
 		} catch (Exception e) {
 			System.out.println(e);
